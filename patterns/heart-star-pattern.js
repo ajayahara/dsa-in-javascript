38f7387dbeeb322b416270538b3d466aec87ab59
ajayahara <abak00350@gmail.com>
2024-02-06 09:19:27 +0530

    update: solved heart star pattern Q.

--- patterns/heart-star-pattern.js
+++ patterns/heart-star-pattern.js
@@ -12,3 +12,33 @@ Q. Print The Follwing Square Pattern
 
 */
 // Solution
+
+function printHeart(){
+     for(let i=2;i<=3;i++){
+          let bag="";
+          for(let j=1;j<=2-i+1;j++){
+               bag+=" ";
+          }
+          for(let j=1;j<=2*i-1;j++){
+               bag+="*"
+          }
+          for(let j=1;j<=2-i+1;j++){
+               bag+=" ";
+          }
+          bag+=" ";
+          bag+=bag;
+          console.log(bag);
+     }
+
+     for(let i=6;i>=1;i--){
+          let bag="";
+          for(let j=1;j<=6-i;j++){
+               bag+=" ";
+          }
+          for(let j=1;j<=2*i-1;j++){
+               bag+="*";
+          }
+          console.log(bag);
+     }
+}
+printHeart();
